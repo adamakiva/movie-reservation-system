7f033c2b53e54b72f43342908ddd09d15420e15e
Adam Akiva <adam.akiva@infinitylabs.co.il>
2024-11-17 15:22:08 +0200

    Working on database schemas

---
+++ src/db/schemas.ts
@@ -0,0 +1,67 @@
+import {
+  index,
+  pgTable,
+  timestamp,
+  uniqueIndex,
+  uuid,
+  varchar,
+} from 'drizzle-orm/pg-core';
+
+/**********************************************************************************/
+
+const timestamps = {
+  createdAt: timestamp('created_at', {
+    mode: 'string',
+    precision: 3,
+    withTimezone: true,
+  })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp('updated_at', {
+    mode: 'string',
+    precision: 3,
+    withTimezone: true,
+  })
+    .defaultNow()
+    .notNull(),
+};
+
+/**********************************************************************************/
+
+const userModel = pgTable(
+  'users',
+  {
+    id: uuid('id').primaryKey().defaultRandom().notNull(),
+    firstName: varchar('first_name').notNull(),
+    lastName: varchar('last_name').notNull(),
+    email: varchar('email').unique().notNull(),
+    hash: varchar('hash').notNull(),
+    roleId: uuid('role_id').references(
+      () => {
+        return roleModel.id;
+      },
+      { onDelete: 'set null', onUpdate: 'cascade' },
+    ),
+    ...timestamps,
+  },
+  (table) => {
+    return [
+      {
+        emailUniqueIndex: uniqueIndex().using('btree', table.email),
+        roleIndex: index().using('btree', table.roleId),
+      },
+    ];
+  },
+);
+
+const roleModel = pgTable(
+  'roles',
+  {
+    id: uuid('id').primaryKey().defaultRandom().notNull(),
+    name: varchar('name').notNull(),
+    ...timestamps,
+  },
+  (table) => {
+    return [{ nameIndex: index().using('btree', table.name) }];
+  },
+);
