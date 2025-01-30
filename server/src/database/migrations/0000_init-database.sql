3871d7aa7d18e0b1d6a77b1e7c3c1703eeddd76a
Adam Akiva <adam.akiva@infinitylabs.co.il>
2025-01-30 14:31:33 +0200

    Removed indexes migration and updated dependencies

--- server/src/database/migrations/0000_init-database.sql
+++ server/src/database/migrations/0000_init-database.sql
@@ -49,8 +49,7 @@ CREATE TABLE "showtime" (
 	"movie_id" uuid NOT NULL,
 	"hall_id" uuid NOT NULL,
 	"created_at" timestamp (3) with time zone DEFAULT now() NOT NULL,
-	"updated_at" timestamp (3) with time zone DEFAULT now() NOT NULL,
-	CONSTRAINT "showtime_at" UNIQUE("at","hall_id")
+	"updated_at" timestamp (3) with time zone DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "user" (
@@ -81,17 +80,4 @@ ALTER TABLE "showtime" ADD CONSTRAINT "showtime_movie_id_movie_id_fk" FOREIGN KE
 ALTER TABLE "showtime" ADD CONSTRAINT "showtime_hall_id_hall_id_fk" FOREIGN KEY ("hall_id") REFERENCES "public"."hall"("id") ON DELETE cascade ON UPDATE cascade;--> statement-breakpoint
 ALTER TABLE "user" ADD CONSTRAINT "user_role_id_role_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."role"("id") ON DELETE no action ON UPDATE cascade;--> statement-breakpoint
 ALTER TABLE "user_showtime" ADD CONSTRAINT "user_showtime_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE cascade;--> statement-breakpoint
-ALTER TABLE "user_showtime" ADD CONSTRAINT "user_showtime_showtime_id_showtime_id_fk" FOREIGN KEY ("showtime_id") REFERENCES "public"."showtime"("id") ON DELETE no action ON UPDATE cascade;--> statement-breakpoint
-CREATE INDEX "movie_genre_id_index" ON "movie" USING btree ("genre_id");--> statement-breakpoint
-CREATE UNIQUE INDEX "movie_cursor_unique_index" ON "movie" USING btree ("id","created_at");--> statement-breakpoint
-CREATE UNIQUE INDEX "role_name_unique_index" ON "role" USING btree ("name");--> statement-breakpoint
-CREATE INDEX "showtime_movie_index" ON "showtime" USING btree ("movie_id");--> statement-breakpoint
-CREATE INDEX "showtime_hall_index" ON "showtime" USING btree ("hall_id");--> statement-breakpoint
-CREATE UNIQUE INDEX "showtime_cursor_unique_index" ON "showtime" USING btree ("id","created_at");--> statement-breakpoint
-CREATE UNIQUE INDEX "user_email_unique_index" ON "user" USING btree ("email");--> statement-breakpoint
-CREATE INDEX "user_role_id_index" ON "user" USING btree ("role_id");--> statement-breakpoint
-CREATE UNIQUE INDEX "user_cursor_unique_index" ON "user" USING btree ("id","created_at");--> statement-breakpoint
-CREATE INDEX "user_showtime_user_index" ON "user_showtime" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "user_showtime_showtime_index" ON "user_showtime" USING btree ("showtime_id");--> statement-breakpoint
-CREATE UNIQUE INDEX "user_showtime_user_showtime_unique_index" ON "user_showtime" USING btree ("showtime_id","user_id");--> statement-breakpoint
-CREATE UNIQUE INDEX "user_showtime_check_reservation_unique_index" ON "user_showtime" USING btree ("showtime_id","row","column");
+ALTER TABLE "user_showtime" ADD CONSTRAINT "user_showtime_showtime_id_showtime_id_fk" FOREIGN KEY ("showtime_id") REFERENCES "public"."showtime"("id") ON DELETE no action ON UPDATE cascade;
